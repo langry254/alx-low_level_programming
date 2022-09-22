4b9085da66935c8b25b2ed5d519f7ded2bc8f97b
langry254 <104241776+langry254@users.noreply.github.com>
2022-09-22 23:53:11 +0300

    a program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - prints the allphabet except q and e
+ *
+ * Return: always 0.
+ */
+int main(void)
+
+{
+		char letter;
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			putchar(letter);
+		}
+		putchar('\n');
+		return (0);
+
+}
