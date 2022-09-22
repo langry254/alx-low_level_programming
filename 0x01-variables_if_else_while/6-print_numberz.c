b251f54dda119e28a5177f42811ac5645923a84b
langry254 <104241776+langry254@users.noreply.github.com>
2022-09-23 00:01:20 +0300

    a program that prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: 0 always
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+	return (0);
+}
