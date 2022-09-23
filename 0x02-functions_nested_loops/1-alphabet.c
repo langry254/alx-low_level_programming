2d687a1aa81c3147e4bbf403aa45e5351b0b0624
langry254 <104241776+langry254@users.noreply.github.com>
2022-09-23 23:07:09 +0300

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,20 @@
 #include "main.h"
+
 /**
- * print_alphabet - main block
+ * print_alphabet - print alphabet lowercase
  *
- * Return: 0 always
-**/
+ * Return: Always 0.
+ */
+
 void print_alphabet(void)
 {
-	char letter;
+char alphabet;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		_putchar(letter);
-	}
-	_putchar('\n');
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
 
 }
